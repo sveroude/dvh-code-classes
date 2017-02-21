533c0f4f5f9d41ee12d5e5bc51d67c9d49b8e232
Sanne Veroude <sanne@voorhoede.nl>
2017-02-21 10:42:17 +0100

    feat(routing): add templates and setup routing

--- app/sample/routes.py
+++ app/sample/routes.py
@@ -19,6 +19,55 @@ def codeClasses():
     codeClasses = session.query(CodeClass).all()
     return render_template('code-classes.html', codeClasses = codeClasses)
 
+@app.route('/code-class/<int:id>/')
+def codeClass(id):
+    codeClass = session.query(CodeClass).filter_by(id = id).one()
+    return render_template('code-class.html', codeClass = codeClass)
+
+@app.route('/code-class/new/', methods=['GET', 'POST'])
+def newCodeClass():
+    if request.method == 'POST':
+        newCodeClass = CodeClass(title=request.form['title'],
+            date=request.form['date'],
+            speaker=request.form['speaker'],
+            description=request.form['description'])
+        session.add(newCodeClass)
+        session.commit()
+        flash('new code class created!')
+        return redirect(url_for('codeClasses'))
+    else:
+        return render_template('new-code-class.html')
+
+@app.route('/code-class/<int:id>/edit/', methods=['GET', 'POST'])
+def editCodeClass(id):
+    editedCodeClass = session.query(CodeClass).filter_by(id = id).one()
+    if request.method == 'POST':
+        if request.form['title']:
+            editedCodeClass.title = request.form['title']
+        if request.form['date']:
+            editedCodeClass.date = request.form['date']
+        if request.form['speaker']:
+            editedCodeClass.speaker = request.form['speaker']
+        if request.form['description']:
+            editedCodeClass.description = request.form['description']
+        session.add(editedCodeClass)
+        session.commit()
+        flash('code class updated!')
+        return redirect(url_for('codeClass', id = id))
+    else:
+        return render_template('edit-code-class.html', codeClass = editedCodeClass)
+
+@app.route('/code-class/<int:id>/delete/', methods=['GET', 'POST'])
+def deleteCodeClass(id):
+    codeClassToDelete = session.query(CodeClass).filter_by(id = id).one()
+    if request.method == 'POST':
+        session.delete(codeClassToDelete)
+        session.commit()
+        flash('code class deleted!')
+        return redirect(url_for('codeClasses'))
+    else:
+        return render_template('delete-code-class.html', codeClass = codeClassToDelete)
+
 # if the script gets executed within the Python interpreter (and not as
 # imported module)
 if __name__ == '__main__':
