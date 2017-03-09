ccd59f72b9020c42cf67564753932deea8cf7982
Sanne Veroude <sanne@voorhoede.nl>
2017-03-09 11:30:02 +0100

    feat(dragend): remove unused script

--- app/sample/src/scripts/dragend.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var component = document.querySelector('[data-dragend]');
-
-  dragend = new Dragend(component, {
-    afterInitialize: function() {
-      component.style.visibility = 'visible';
-    }
-  });
-}, false)
